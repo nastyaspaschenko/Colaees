cc2b02cbb6462db1a48e23dd03b8f93dd10f966a
nastyaspaschenko <nastya.spaschenko@gmail.com>
2020-10-14 05:41:36 +0300

    fixed

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const $burger = $('.burger');
+
 const $sliderNav = $('.slider_nav_item');
 const $slides = $('.slide');
 
-$burger.click(function () { 
-    $burger.toggleClass('active');
-    $('.mobile_nav_menu').slideToggle();
-});
-$('.mobile_nav_menu .sub_menu_title').click(function () { 
-    $('.mobile_nav_menu .sub_menu_items').slideToggle();
-});
-$('.mobile_nav_menu .sub_sub_menu_title').click(function () { 
-    $('.mobile_nav_menu .sub_sub_menu_items').slideToggle();
-});
+
+
+if(window.matchMedia('(max-width: 767px)').matches) {
+    const $burger = $('.burger');
+    $burger.click(function () {
+        $burger.toggleClass('active');
+        $('.nav_menu').slideToggle();
+    });
+    $('.nav_menu .sub_menu_title').click(function () { 
+        $('.nav_menu .sub_menu_items').slideToggle();
+    });
+    $('.nav_menu .sub_sub_menu_title').click(function () { 
+        $('.nav_menu .sub_sub_menu_items').slideToggle();
+    });
+} else {
+    $('.nav_menu .sub_menu').hover(function () {
+            $('.nav_menu .sub_menu_items').slideToggle();
+        }
+    );
+    $('.nav_menu .sub_sub_menu').hover(function () {        
+        $('.nav_menu .sub_sub_menu_items').fadeToggle(300);
+        $('.nav_menu .sub_sub_menu').toggleClass('active');
+    }
+);
+}
+
 
 $sliderNav.click(function() {
     showSlide($sliderNav.index(this));
